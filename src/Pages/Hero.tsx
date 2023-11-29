6411bd535f3c7f4d300596f848608cb2da7ab8da
Okwoli-Godwin <okwolig60@gmail.com>
2023-11-29 03:17:29 +0100

    pushed

--- src/Pages/Hero.tsx
+++ src/Pages/Hero.tsx
@@ -1,11 +1,21 @@
-
+import { Typewriter } from 'react-simple-typewriter';
 
 const Hero = () => {
   return (
     <div className="w-[100%] flex bg-hero bg-center bg-cover bg-no-repeat">
-        <div className="w-[100%] flex bg-[rgba(0,0,0,0.6)] pt-[180px] pb-[180px] justify-center">
+        <div className="w-[100%] flex bg-[rgba(0,0,0,0.7)] pt-[180px] pb-[180px] justify-center">
           <div className="w-[95%] flex flex-col items-center">
-          <h1 className="text-[#fff] text-[55px] w-[450px] text-center">Business Payments Made Simple</h1>
+          <h1 className="text-[#fff] text-[55px] w-[480px] text-center">Business Payments Made
+            <span style={{color: "#0B73F7", marginLeft: "15px"}}>
+              <Typewriter
+                words={['Easy', 'Simple', 'Safe']}
+                loop
+                cursor
+                typeSpeed={500}
+                delaySpeed={150}
+              />
+            </span>
+          </h1>
           <p className="text-[#fff] w-[570px] text-center mt-[23px] text-[18px]">
             Get a unique bank account number that enables your company to pay
             employees' salaries through the wallet.
