1494d61a8bcbac496bd13697ad3fff24571bdf8d
insignamedia <insignamedia@gmail.com>
2023-11-30 16:57:55 +0100

    working on transaction page

--- src/Pages/Admindashboardpage/TransactionHistory.tsx
+++ src/Pages/Admindashboardpage/TransactionHistory.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import TransationTable from '../../Components/blocks/admin/TransationTable'
 
 const TransactionHistory:React.FC = () => {
   return (
-    <div className='w-full h-[10vh] bg-slate-400'>TransactionHistory</div>
+    <div className='w-full min-h-screen h-full bg-slate-400'>
+        {/* user table */}
+        <TransationTable/>
+    </div>
   )
 }
 
