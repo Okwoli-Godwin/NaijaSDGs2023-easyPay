98f48bbe484271134a8466e1bd2a31103665ef8b
Okwoli-Godwin <okwolig60@gmail.com>
2023-12-01 13:04:21 +0100

    reduxState added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { Provider } from "react-redux/es/exports"
+import { Store } from './Components/Global/Store.tsx'
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+const queryClient = new QueryClient();
+
+let persistor = persistStore(Store)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={Store}>
+      <PersistGate persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools />
+        </QueryClientProvider>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
