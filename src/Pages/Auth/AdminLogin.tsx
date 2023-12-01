631f092974fcbb16a92a36c0b416a32c3a0e7664
Okwoli-Godwin <okwolig60@gmail.com>
2023-12-01 09:37:33 +0100

    update

--- src/Pages/Auth/AdminLogin.tsx
+++ src/Pages/Auth/AdminLogin.tsx
@@ -1,7 +1,7 @@
 import img from "../../assets/adminsignupimg.png"
 import img2 from "../../assets/pay.png"
 
-const StaffSignup = () => {
+const AdminLogin = () => {
   return (
     <div className="w-[100%] flex justify-center">
         <div className="w-[100%] flex">
@@ -18,33 +18,28 @@ const StaffSignup = () => {
                 <p className="w-[480px] text-center mt-[20px]">Get a unique bank account number that  enables your company to pay   employees' salaries through the wallet.</p>
                 
                 <div className="w-[100%] flex flex-col mt-[40px]">
-                    <h3>Name</h3>
-                    <input type="text" className="w-[100%] h-[43px] rounded-[3px] mt-[5px] pl-[12px] outline-none border border-gray-300" placeholder="FullName"/>
+                    <h3>Company Name</h3>
+                    <input type="text" className="w-[100%] h-[43px] rounded-[3px] mt-[5px] pl-[12px] outline-none border border-gray-300" placeholder="my company"/>
                 </div>
                 <div className="w-[100%] flex flex-col mt-[20px]">
-                    <h3>Email</h3>
-                    <input type="email" className="w-[100%] h-[43px] rounded-[3px] mt-[5px] pl-[12px] border border-gray-300 outline-none" placeholder="myemail@gmail.com"/>
-                </div>
-                <div className="w-[100%] flex flex-col mt-[20px]">
-                    <h3>Company's Name</h3>
-                    <input type="text" className="w-[100%] h-[43px] rounded-[3px] mt-[5px] pl-[12px] border border-gray-300 outline-none" placeholder="my company"/>
-                </div>
-                <div className="w-[100%] flex flex-col mt-[20px]">
-                    <h3>Position</h3>
-                    <input type="text" className="w-[100%] h-[43px] rounded-[3px] mt-[5px] pl-[12px] border border-gray-300 outline-none" placeholder="staff"/>
+                    <h3>Company Email</h3>
+                    <input type="email" className="w-[100%] h-[43px] rounded-[3px] mt-[5px] pl-[12px] border border-gray-300 outline-none" placeholder="mycompany@gmail.com"/>
                 </div>
                 <div className="w-[100%] flex flex-col mt-[20px]">
                     <h3>Password</h3>
                     <input type="password" className="w-[100%] h-[43px] rounded-[3px] mt-[5px] pl-[12px] border border-gray-300 outline-none" placeholder="password"/>
                 </div>
+                <div className="w-[100%] mt-[15px] cursor-pointer">
+                    <h4 className="text-[14px]">Forget password? <span className="text-[#716DF2]">click here</span></h4>
+                </div>
                 <button className="w-[100%] h-[43px] bg-[#888DF2] rounded-[4px] flex justify-center items-center text-[#fff] mt-[25px]">
-                    Sign Up
+                    Sign In
                 </button>
-                <h4 className="mt-[10px]">Already have an account? <span className="text-[#716DF2]">Sign in instaed</span></h4>
+                <h4 className="mt-[10px]">Don't have an account? <span className="text-[#716DF2]">Sign up</span></h4>
             </div>
         </div>
     </div>
   )
 }
 
-export default StaffSignup
+export default AdminLogin
