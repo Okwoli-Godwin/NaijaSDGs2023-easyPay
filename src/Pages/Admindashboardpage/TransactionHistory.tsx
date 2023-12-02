a1e3410d6b3059e09050582f29887d51ed4678f7
victor-okereke <okerekev324@gmail.com>
2023-12-01 18:46:45 -0800

    edited admin transactions history again

--- src/Pages/Admindashboardpage/TransactionHistory.tsx
+++ src/Pages/Admindashboardpage/TransactionHistory.tsx
@@ -1,21 +1,16 @@
-import React from 'react'
-import TransationTable from '../../Components/blocks/admin/TransationTable'
-import avatar from "../../assets/avatar.png"
+import React from "react";
+import TransationTable from "../../Components/blocks/admin/TransationTable";
 
 const TransactionHistory: React.FC = () => {
   return (
-<<<<<<< HEAD
-    <div className="w-full min-h-screen h-full flex justify-center">
-      {/* user table */}
-      <div className="w-[92%]">
-        <TransationTable />
-      </div>
-=======
-    <div className='w-full min-h-screen h-full bg-slate-400'>
+    <>
+      <div className="w-full min-h-screen h-full flex justify-center">
         {/* user table */}
-        <TransationTable tableName='History' img={avatar}/>
->>>>>>> 6636a30d583db96c8c7a7beea69f61c813296ecf
-    </div>
+        <div className="w-[92%]">
+          <TransationTable />
+        </div>
+      </div>
+    </>
   );
 };
 
