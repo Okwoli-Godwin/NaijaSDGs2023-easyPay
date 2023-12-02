610196a2e8c14e2cff6f5121fb12504918ae1bc8
insignamedia <insignamedia@gmail.com>
2023-12-02 23:52:42 +0100

    added financial page

---
+++ src/Components/blocks/staffDashbord/finance/Title.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Title:React.FC = () => {
+  return (
+    <div>
+        <h2 className='text-2xl font-semibold'>Hello dear,</h2>
+        <h3 className='text-xl'>I am your AI financial advisor 🤖</h3>
+    </div>
+  )
+}
+
+export default Title
